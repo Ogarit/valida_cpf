32b8c884ca3f1cb9522c0a91b3ac1e4e3c514521
Tiago Marques Lima <118786072+Ogarit@users.noreply.github.com>
2023-09-06 17:19:18 -0300

    Atualizar o connection.py

--- validacpf/connection.py
+++ validacpf/connection.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine(f'mysql+pymysql://{"root"}:{""}*@localhost/cpf')
+engine = create_engine(f'mysql+pymysql://{"root"}:{""}@{"localhost"}:{"3306"}/{"cpf"})
 
 Base = declarative_base()
 
